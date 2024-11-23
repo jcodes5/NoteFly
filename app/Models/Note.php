e211f5998307b04733f5fba91ca553db055565cd
jcodes5 <jattodare002@gmail.com>
2024-11-23 12:14:12 +0100

    Worked on the models and migrations

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,5 +44,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+
     }
+    public function notes() {
+        return $this->hasMany(Note::class);
+    }
+
+
 }
